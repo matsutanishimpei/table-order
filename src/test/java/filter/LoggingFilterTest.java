be1773a80ecc98a97c0a07f61c0baa3289c0abcc
smatsutani <dear.old.days@gmail.com>
2026-07-20 12:05:21 +0900

    test: initialize logging filter mocks

--- src/test/java/filter/LoggingFilterTest.java
+++ src/test/java/filter/LoggingFilterTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class LoggingFilterTest {
 
     @Mock
